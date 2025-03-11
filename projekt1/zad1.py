f9629d501fb2013771b445c27484a8b49951aaf3
WsadTraversy <64471293+WsadTraversy@users.noreply.github.com>
2025-03-11 17:07:43 +0100

    proj1

---
+++ projekt1/zad1.py
@@ -0,0 +1,78 @@
+import math
+import random
+import numpy as np
+
+def function(z: list):
+    x, y = z
+    return 9*x*y / math.e**(x**2 + 1/2*x + y**2)
+def dfdx(z: list):
+    x, y = z
+    return (-18*x**2 - 4.5*x + 9)*y / math.e**(x**2 + x/2 + y**2)
+def dfdy(z: list):
+    x, y = z
+    return -9*x * (2*y**2 - 1) / math.e**(x**2 + x/2 + y**2)
+def gradient(z: list):
+    return [dfdx(z), dfdx(z)]
+def gradient_x(z: list):
+    return dfdx(z)
+def gradient_y(z: list):
+    return dfdy(z)
+
+def sgd_minimum(initial: list, alpha=0.1, steps=1000):
+    x_n = list(initial)
+    grad = np.array(gradient(x_n))
+    diff = -alpha * grad
+    for _ in range(steps):
+        rand = random.randint(0, 1)
+        if rand == 1:
+            diff[0] = -alpha * gradient_x(x_n)
+            x_n += diff
+        else: 
+            diff[1] = -alpha * gradient_y(x_n)
+            x_n += diff
+        if np.all(np.abs(diff) <= 1e-06):
+            break
+    return x_n
+
+def sgd_maximum(initial: list, alpha=0.1, steps=1000):
+    x_n = list(initial)
+    grad = np.array(gradient(x_n))
+    diff = alpha * grad
+    for _ in range(steps):
+        rand = random.randint(0, 1)
+        if rand == 1:
+            grad = np.array(gradient(x_n))
+            diff = alpha * grad
+            x_n += diff
+        else: 
+            x_n += diff
+        if np.all(np.abs(diff) <= 1e-06):
+            break
+    return x_n
+
+print("Punkt początkowy: [0.6, 0.71]")
+print(sgd_minimum([0.6, 0.71]))
+# print("Punkt początkowy: [1, 0.71]")
+# print(sgd_minimum([1, 0.71]))
+# print("Punkt początkowy: [-0.9, -0.8]")
+# print(sgd_minimum([-0.9, -0.8]))
+# print("Punkt początkowy: [-0.85, -0.79]")
+# print(sgd_minimum([-0.85, -0.79]))
+# print("Punkt początkowy: [-2, 2]")
+# print(sgd_minimum([-2, 2]))
+# print("Punkt początkowy: [1, -0.71]")
+# print(sgd_minimum([1, -0.71]))
+
+# print("Punkt początkowy: [1, -2]")
+# print(sgd_maximum([1, -2]))
+# print("Punkt początkowy: [2, -2]")
+# print(sgd_maximum([2, -2]))
+# print("Punkt początkowy: [-2, 1]")
+# print(sgd_maximum([-2, 1]))
+# print("Punkt początkowy: [-1, 1]")
+# print(sgd_maximum([-1, 1]))
+# print("Punkt początkowy: [0, 1]")
+# print(sgd_maximum([0, 1]))
+# print("Punkt początkowy: [-1, 0]")
+# print(sgd_maximum([-1, 0]))
+# print(sgd_maximum([-5, -5]))

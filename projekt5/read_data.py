e8e3c747c0efde2e6365ba2c986b5229a6d1b03f
WsadTraversy <64471293+WsadTraversy@users.noreply.github.com>
2025-03-11 17:25:40 +0100

    tail

--- projekt5/read_data.py
+++ projekt5/read_data.py
@@ -164,83 +164,17 @@ def get_data_with_tail(file_name: str):
     return np.ndarray(shape=(length_x, 11), dtype=int, buffer=list_of_data), np.ndarray(shape=(length_y, 4), dtype=int, buffer=y_data)
 
 def game_state_to_data_sample_tail(game_state):
-    list_of_data = list()
-    food = game_state.get('food')
-    head = game_state.get('snake_body')[-1]
-    food_dict = {'food_up': 0, 'food_right': 0, 'food_down': 0, 'food_left': 0}
-
-    if head[1] - food[1] > 0:
-        food_dict.update({'food_up': 1})
-    if head[0] - food[0] < 0:
-        food_dict.update({'food_right': 1})
-    if head[1] - food[1] < 0:
-        food_dict.update({'food_down': 1})
-    if head[0] - food[0] > 0:
-        food_dict.update({'food_left': 1})
-
-    collision_dict = {'collision_up': 0, 'collision_right': 0, 'collision_down': 0, 'collision_left': 0}
-
-    if head[1] == 0:
-        collision_dict.update({'collision_up': 1})
-    if head[0] == 270:
-        collision_dict.update({'collision_right': 1})
-    if head[1] == 270:
-        collision_dict.update({'collision_down': 1})
-    if head[0] == 0:
-        collision_dict.update({'collision_left': 1})
-
-    tail_forward = 0
-    tail_right  = 0
-    tail_left = 0 
-    body_values = list()
-    head = game_state["snake_body"][-1]
-    for element in game_state["snake_body"][:-1]:
-        body_values.append((element[0], element[1]))
-    state1 = head[0] + 30
-    state2 = head[0] - 30
-    state3 = head[1] + 30
-    state4 = head[1] - 30
-
-    if game_state["snake_direction"].value == 0: # up
-        if (head[0], state4) in body_values:
-            tail_forward = 1
-        if (state1, head[1]) in body_values:
-            tail_right = 1
-        if (state2, head[1]) in body_values:
-            tail_left = 1
-    
-    if game_state["snake_direction"].value == 1: # right
-        if (state1, head[1]) in body_values:
-            tail_forward = 1
-        if (head[0], state3) in body_values:
-            tail_right = 1
-        if (head[0], state4) in body_values:
-            tail_left = 1
-
-    if game_state["snake_direction"].value == 2: # down
-        if (head[0], state3) in body_values:
-            tail_forward = 1
-        if (state1, head[1]) in body_values:
-            tail_left = 1
-        if (state2, head[1]) in body_values:
-            tail_right = 1
-
-    if game_state["snake_direction"].value == 3: # left
-        if (state2, head[1]) in body_values:
-            tail_forward = 1
-        if (head[0], state3) in body_values:
-            tail_left = 1
-        if (head[0], state4) in body_values:
-            tail_right = 1
-
     row_list = list()
-    row_list += list(food_dict.values())
-    row_list += list(collision_dict.values())
-    row_list += list( {'tail_forward': tail_forward, 'tail_right': tail_right, 'tail_left': tail_left}.values())
+    list_of_data = list()
+    
+    row_list += list(food_direction(game_state).values())
+    row_list += list(collision(game_state).values())
+    row_list += list(tail(game_state).values())
     list_of_data.append(row_list)
-
+    row_list = list()
+        
     length_x = len(list_of_data)
     list_of_data = np.array(list_of_data)
     
-    data =  np.ndarray(shape=(length_x, 11), dtype=int, buffer=list_of_data)
+    data =  np.ndarray(shape=(length_x,11), dtype=int, buffer=list_of_data)
     return torch.from_numpy(data).float()
